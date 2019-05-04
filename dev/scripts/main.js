3112f8bae7f1d7262918cdd08aecde55cbdc5c58
KaydeeDee <kd.dangelo@gmail.com>
2019-05-04 16:10:02 -0400

    svg on main page sizing fixed, spacing adjusted

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,4 +1,34 @@
 const lightbulbBottomSVG = document.querySelector('.lightbulb-bottom');
 const test = lightbulbBottomSVG.getTotalLength();
 
-console.log(test);
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navAnchors = document.querySelector('.test');
+    console.log(navAnchors);
+    const lightBulbCoil = document.querySelector('.lightbulb-coil');
+
+    navAnchors.addEventListener("mouseover", mouseOver);
+    navAnchors.addEventListener("mouseleave", mouseLeave);
+
+    function mouseOver() {
+        lightBulbCoil.classList.add('red');
+    }
+
+    function mouseLeave() {
+        lightBulbCoil.classList.remove('red');
+    }
+
+
+    // const mouseLeave = () => {
+    //     lightBulbCoil.classList.remove('red');
+    //     console.log('out');
+    // }
+
+});
+
+
+
+// navAnchors.addEventListener("mouseover", function(event) {
+//     lightBulbCoil.target.style.stroke = "blue";
+// })
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,4 +2,24 @@
 
 var lightbulbBottomSVG = document.querySelector('.lightbulb-bottom');
 var test = lightbulbBottomSVG.getTotalLength();
-console.log(test);
+document.addEventListener("DOMContentLoaded", function () {
+  var navAnchors = document.querySelector('.test');
+  console.log(navAnchors);
+  var lightBulbCoil = document.querySelector('.lightbulb-coil');
+  navAnchors.addEventListener("mouseover", mouseOver);
+  navAnchors.addEventListener("mouseleave", mouseLeave);
+
+  function mouseOver() {
+    lightBulbCoil.classList.add('red');
+  }
+
+  function mouseLeave() {
+    lightBulbCoil.classList.remove('red');
+  } // const mouseLeave = () => {
+  //     lightBulbCoil.classList.remove('red');
+  //     console.log('out');
+  // }
+
+}); // navAnchors.addEventListener("mouseover", function(event) {
+//     lightBulbCoil.target.style.stroke = "blue";
+// })
